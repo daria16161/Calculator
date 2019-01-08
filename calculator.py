5c1d7179c85807ff046ae828afaa04d990ba5476
Aleksander Jastrzębski <36478091+kisamm@users.noreply.github.com>
2019-01-08 01:38:46 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,80 @@
+import math
+
+while True:
+
+    user = input("This is a calculator what do you wanna do?: multiply, divide, add, subtract, percent, pi, eulier, squere, squere root? m/d/a/s/p/pi/e/sq/sqr ")
+
+    if user == "p":
+        print("You count ..% from a value")
+    if user == "sqr":
+        num4 = int(input("Give mi a number "))
+
+    num1 = int(input("Give your first number "))
+    num2 = int(input("Give your secound number "))
+
+# def function
+
+    def multiply(num1,num2):
+        return num1*num2
+    result = multiply(num1,num2)
+
+    def divide(num1,num2):
+        return num1/num2
+    result1 = divide(num1,num2)
+
+    def add(num1,num2):
+        return num1+num2
+    result2 = add(num1,num2)
+
+    def subtract(num1,num2):
+        return num1-num2
+    result3 = subtract(num1,num2)
+
+    def percent(num1,num2):
+        return num1/100*num2
+    result4 = percent(num1,num2)
+
+#EULIER You only have to write your value in num1, in num2 it's whatever you want
+
+    def eulier(num1):
+        return math.sqrt(num1)
+    result5 = eulier(num1)
+#PI You only have to write your value in num1, in num2 it's whatever you want
+    def pi(num1):
+        return num1*3.14
+    result6 = pi(num1)
+
+# SQUERE You only have to write your value in num1, in num2 it's whatever you want
+    def squere(num1):
+        return (num1**2)
+    result7 = squere(num1)
+
+    def squereroot(num4,num1,num2):
+        return (num1**2)-4*(num4*num2)
+    result8 = squereroot(num4,num1,num2)
+
+#Below are the command who giving a results
+
+    if user == "m":
+        print("Multiply result is:",result)
+    elif user == "d":
+        print("Divide result is:",result1)
+    elif user == "a":
+        print("Add result is:",result2)
+    elif user == "s":
+        print("Subtract result is:",result3)
+    elif user == "p":
+        print("Percent result is:",result4)
+    elif user == "e":
+        print("Eulier result is:",result5)
+    elif user == "pi":
+        print("PI result is: ",result6)
+    elif user == "sq":
+        print("Squere is:",result7)
+    elif user == "sqr":
+        if result8 < 0:
+            print("Delta can't be negative")
+            break
+        else:
+            print(result8)
+
